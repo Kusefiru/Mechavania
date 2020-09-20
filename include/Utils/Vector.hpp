34c5b815505f71ee1fba5bbcf7e85f50752d0d2d
PacificVoid <pacificvoid@netc.eu>
2020-09-20 13:22:23 +0200

    Add += and -= to Utils/Vector.hpp

--- include/Utils/Vector.hpp
+++ include/Utils/Vector.hpp
@@ -19,9 +19,17 @@ inline Vector2<T> operator-(const Vector2<T>& v) { return Vector2<T>(-v.x, -v.y)
 template<typename T>
 inline Vector2<T> operator+(const Vector2<T>& l, const Vector2<T>& r) { return Vector2<T>(l.x + r.x, l.y + r.y);}
 
+// l += r
+template<typename T>
+inline Vector2<T>& operator+=(Vector2<T>& l, const Vector2<T>& r) { l.x += r.x; l.y += r.y; return l; }
+
 // l - r
 template<typename T>
 inline Vector2<T> operator-(const Vector2<T>& l, const Vector2<T>& r) { return Vector2<T>(l.x - r.x, l.y - r.y);}
+
+// l -= r
+template<typename T>
+inline Vector2<T>& operator-=(Vector2<T>& l, const Vector2<T>& r) { l.x -= r.x; l.y -= r.y; return l; }
 
 // l == r ?
 template<typename T>
